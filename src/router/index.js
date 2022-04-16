d93c9f5b1c605a8cdce8512ba3d98d1e07a10b13
fan9704 <cxz123499@gmail.com>
2022-04-16 23:02:17 +0800

    Create Vue-router

---
+++ src/router/index.js
@@ -0,0 +1,51 @@
+import { createRouter } from "vue-router";
+// import Login from "../components/Login.vue";
+// import Index from "../components/Index.vue"
+// import Certilist from "../views/Certilist.vue"
+// import Settings from "../components/Settings.vue"
+// import ChatroomView from "../views/ChatRoom.vue"
+// import FileConverterView from "../views/FileConverter.vue"
+// import GraphView from "../components/Graph.vue"
+const routes = [
+    // {
+    //         path: "/",
+    //         name: "index",
+    //         component: Index,
+    //     },
+    //     {
+    //         path: "/account/login",
+    //         name: "Login",
+    //         component: Login,
+    //     },
+    //     {
+    //         path: "/certification/list",
+    //         name: "Certilist",
+    //         component: Certilist,
+    //     },
+    //     {
+    //         path: "/account/setting",
+    //         name: "Settings",
+    //         component: Settings,
+    //     },
+    //     {
+    //         path: "/chat",
+    //         name: "ChatroomView",
+    //         component: ChatroomView,
+    //     },
+    //     {
+    //         path: "/file",
+    //         name: "FileConverterView",
+    //         component: FileConverterView,
+    //     },
+    //     {
+    //         path: "/graph",
+    //         name: "GraphView",
+    //         component: GraphView,
+    //     },
+];
+
+const router = createRouter({
+    routes,
+});
+
+export default router;
