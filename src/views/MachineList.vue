8f08a50bc86571fe96f8d40969f8aac6bfee04e4
fan9704 <cxz123499@gmail.com>
2022-04-24 13:46:19 +0800

    Create Edit Machine Information Form

--- src/views/MachineList.vue
+++ src/views/MachineList.vue
@@ -13,8 +13,53 @@
     </v-dialog>
     <!--MachineInfoForm  -->
     <v-container v-show="MachineInfoForm" class="MachineInfoForm">
+      <v-row
+        ><h2>Edit Machine Information</h2>
+        <v-btn class="closeButton" color="error" size="small" @click="MachineInfoForm = !MachineInfoForm" >X </v-btn></v-row
+      >
       <v-row>
-        <v-col cols="1"></v-col>
+        <v-col cols="3" xs="3" sm="3" md="1" lg="1">Machine Location</v-col>
+        <v-col cols="9" xs="9" sm="9" md="11" lg="11">
+          <v-text-field
+            label="Machine Location"
+            variant="outlined"
+            v-model="MachineLocation"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="3" xs="3" sm="3" md="1" lg="1">User Lock</v-col>
+        <v-col cols="9" xs="9" sm="9" md="11" lg="11">
+          <v-text-field
+            label="User Lock"
+            variant="outlined"
+            v-model="UserLock"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="3" xs="3" sm="3" md="1" lg="1">Machine Lock</v-col>
+        <v-col cols="9" xs="9" sm="9" md="11" lg="11">
+          <v-text-field
+            label="Machine Lock"
+            variant="outlined"
+            v-model="MachineLock"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="3" xs="3" sm="3" md="1" lg="1">Current User</v-col>
+        <v-col cols="9" xs="9" sm="9" md="11" lg="11">
+          <v-text-field
+            label="Current User"
+            variant="outlined"
+            v-model="CurrentUser"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row >
+        <v-col cols="6" class="d-flex mb-6 justify-space-around"><v-btn color="secondary">Check User Valid</v-btn></v-col>
+         <v-col cols="6" class="d-flex mb-6 justify-space-around"><v-btn color="success" :disabled="MachineInfoBtn">Edit Machine Information</v-btn></v-col>
       </v-row>
     </v-container>
     <!--MachineInfoForm  -->
@@ -35,10 +80,16 @@
         </select> -->
       </v-col>
       <v-col cols="5" xs="12" sm="12" md="12" lg="5" class="d-flex">
-        <v-text-field label="Machine Location" placeholder="Placeholder" v-model="CreateMachineLocation"></v-text-field
+        <v-text-field
+          label="Machine Location"
+          placeholder="Placeholder"
+          v-model="CreateMachineLocation"
+        ></v-text-field
       ></v-col>
-      <v-col cols="1"  xs="12" sm="12" md="12" lg="1" class="d-flex">
-         <v-btn flat color="success" @click="createMachine">Create Machine</v-btn>
+      <v-col cols="1" xs="12" sm="12" md="12" lg="1" class="d-flex">
+        <v-btn flat color="success" @click="createMachine"
+          >Create Machine</v-btn
+        >
       </v-col>
     </v-row>
     <v-row>
@@ -49,7 +100,12 @@
       <v-col cols="2" sm="4" md="2"> Current User </v-col>
       <v-col cols="2" sm="4" md="2"> Operation </v-col>
     </v-row>
-    <v-row :key="index" v-for="(cert, index) in certs" class="certs-row" data-aos="flip-right">
+    <v-row
+      :key="index"
+      v-for="(cert, index) in certs"
+      class="certs-row"
+      data-aos="flip-right"
+    >
       <v-col cols="2" sm="4" md="2" lg="2" data-aos="flip-right">
         <!-- {{index + 1 + (page - 1) * select_option}} -->
         {{ cert.id }}
@@ -60,7 +116,7 @@
       <v-col cols="2" sm="4" md="2" lg="2" data-aos="flip-right">
         {{ cert.user_lock }}
       </v-col>
-      <v-col cols="2" sm="4" md="2" lg="2" >
+      <v-col cols="2" sm="4" md="2" lg="2">
         {{ cert.machine_lock }}
       </v-col>
       <v-col
@@ -80,7 +136,9 @@
         </span>
       </v-col>
       <v-col cols="2" sm="4" md="2" data-aos="flip-right">
-        <v-btn flat color="warning" @click="showMachineForm(cert.id)">Edit Info</v-btn>
+        <v-btn flat color="warning" @click="showMachineForm(cert.id)"
+          >Edit Info</v-btn
+        >
       </v-col>
     </v-row>
   </v-container>
@@ -106,8 +164,13 @@ export default {
       datanumber: 100,
       page: 1,
       progress: true,
-      CreateMachineLocation:"",
-      MachineInfoForm:false,
+      CreateMachineLocation: "",
+      MachineInfoForm: false,
+      MachineLocation: "",
+      UserLock: "",
+      MachineLock: "",
+      CurrentUser: "",
+      MachineInfoBtn:true,
     };
   },
   methods: {
@@ -123,32 +186,43 @@ export default {
         })
         .catch((error) => console.log(error));
     },
-    createMachine(){
-      let url="/api/machine";
-      let config={
+    createMachine() {
+      let url = "/api/machine";
+      let config = {
         location: this.CreateMachineLocation,
         machine_lock: false,
-        user_lock: false
-      }
-      if(this.CreateMachineLocation!=""){
-        this.axios.post(url,config)
-        .then((res)=>{
-          this.$swal.fire(  'Create Machine Success!!',`${this.CreateMachineLocation} ~ `,'success');
-        })
-      }else{
-        this.$swal.fire(  'Create Failed!!',`Please Enter Location Again `,'error');
+        user_lock: false,
+      };
+      if (this.CreateMachineLocation != "") {
+        this.axios.post(url, config).then((res) => {
+          this.$swal.fire(
+            "Create Machine Success!!",
+            `${this.CreateMachineLocation} ~ `,
+            "success"
+          );
+        });
+      } else {
+        this.$swal.fire(
+          "Create Failed!!",
+          `Please Enter Location Again `,
+          "error"
+        );
       }
     },
-    showMachineForm(id){
+    showMachineForm(id) {
       let url = `/api/machine/${id}`;
       this.axios
         .get(url)
         .then((res) => {
-          this.MachineInfoForm= !this.MachineInfoForm;
+          this.MachineInfoForm = !this.MachineInfoForm;
           console.log(res.data);
+          this.MachineLocation = res.data.location;
+          this.UserLock = res.data.user_lock;
+          this.MachineLock = res.data.machine_lock;
+          this.CurrentUser = res.data.current_user.userName;
         })
         .catch((error) => console.log(error));
-    }
+    },
   },
   beforeMount() {
     let url = "/api/machines/";
@@ -182,5 +256,24 @@ option {
   background: linear-gradient(rgb(255, 210, 151), rgb(248, 236, 169));
   border-radius: 5px;
   margin: 15px 0px;
+}
+.MachineInfoForm {
+  border-radius: 10px;
+  border: solid 3px rgb(243, 180, 64);
+  /* background: linear-gradient(rgb(255, 255, 255), rgb(240, 196, 116)); */
+  background-color: rgb(253, 252, 230);
+  padding: 30px;
+  position: absolute;
+  z-index: 2;
+}
+.MachineInfoForm h2 {
+  margin: 15px;
+  width: 100%;
+  text-align: center;
+}
+.MachineInfoForm .closeButton {
+  position: absolute;
+  right: 0;
+  top: 0;
 }
 </style>
